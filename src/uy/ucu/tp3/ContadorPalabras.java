b0e04b7697377f85991cc5fe3f8f94eb41895dd0
macarenapineiro <macarena.pineiro@correo.ucu.edu.uy>
2025-03-29 19:26:57 -0300

    Arreglos finales en tp3

--- src/uy/ucu/tp3/ContadorPalabras.java
+++ src/uy/ucu/tp3/ContadorPalabras.java
@@ -8,7 +8,7 @@ import java.util.List;
 /**
  * Clase que permite contar la cantidad de palabras, vocales y consonantes en una frase,
  * además de determinar cuántas palabras tienen más letras que un valor mínimo especificado.
- * También proporciona métodos para leer líneas desde un archivo.
+ * También proporciona métodos para leer líneas desde un archivo y contar la cantidad de palabras que contiene.
  * 
  * @author Macarena
  * @version 1.0
@@ -17,58 +17,83 @@ public class ContadorPalabras {
 
 //JavaDoc del metodo, documentacion
 	/**
-	 * Cuenta la cantidad de palabras en la frase proporcionada, así como la cantidad de vocales y consonantes.
-     * Además, determina cuántas palabras tienen más letras que el número mínimo especificado.
+	 * Cuenta la cantidad de palabras en la frase proporcionada
      * 
 	 * @param frase La frase a contar
-	 * @param numero El numero minimo de letras de una palabra larga que se quiera
 	 */
-	public void contarPalabras(String frase, int numero) {
-		int palabras = 0;
-		int espacios = 0;
-		int vocales = 0;
-		int consonantes = 0;
-		int palabrasLargas = 0;
-		int contadorLetras = 0;
-		
-		if(frase.charAt(0) == ' ') {
-			frase = frase.substring(1);
-		}
-		if(frase.charAt(frase.length()-1 ) == ' ') {
-			frase.substring(0, frase.length()-1);
-		}
-		frase.toLowerCase();
-		for(int i = 0; i < frase.length(); i++) {
-			if(Character.isLetter(frase.charAt(i))) {
-				if(esVocal(frase.charAt(i))) {
-					vocales ++;
-				} else {
-					consonantes ++;
-
-				}
-				contadorLetras ++;
-			}
-			
-			if(frase.charAt(i) == ' ') {
-				espacios ++;
-				if(contadorLetras > numero) {
-					palabrasLargas ++;
-				}
-				contadorLetras = 0;
-			}
-			
-		}
-		if(contadorLetras > numero) {
-			palabrasLargas ++;
-		}
-		palabras = espacios + 1;
-		System.out.println("Cantidad de palabras: " + palabras);
-		System.out.println("Cantidad de vocales: " + vocales);
-		System.out.println("Cantidad de consonantes: " + consonantes);
-		System.out.println("Cantidad de palabras mas largas que " + numero+ " letras: " + palabrasLargas);
-
-
-	}
+	public int contarPalabras(String frase) {
+ 		int palabras = 0;
+ 		int espacios = 0;
+ 		if(frase == null || frase.isBlank()) {
+ 			return 0;
+ 		}
+ 		if(frase.charAt(0) == ' ') {
+ 			frase = frase.substring(1); 	
+ 		}
+ 		if(frase.length()>0 && frase.charAt(frase.length()-1) == ' ') {
+ 			frase = frase.substring(0, frase.length()-1);
+ 		}
+ 		for (int i = 0; i < frase.length(); i++) {
+ 			if(frase.charAt(i) == ' ') {
+ 				espacios += 1;
+ 			}
+ 		}
+ 		palabras = espacios + 1;
+ 		
+ 		
+ 		
+ 		return palabras;
+ 	}
+	/**
+	 * Cuenta la cantidad de vocales y consonantes de una frase dada
+	 * 
+	 * @param frase La frase en la que se cuentan las letras
+	 * @return Un string con la información sobre el resultado obtenido de vocales y consonantes
+	 */
+ 	public String cantidadLetras(String frase) {
+ 		int vocales = 0;
+ 		int consonantes = 0;
+ 		for (int i = 0; i < frase.length(); i++) {
+ 			if(Character.isLetter(frase.charAt(i))) {
+ 				if(esVocal(frase.charAt(i)))
+ 				{
+ 					vocales += 1;
+ 				}
+ 				else {
+ 				consonantes += 1;	
+ 				}
+ 			}
+ 				
+ 		}
+ 		return "Cantidad vocales: " + vocales + ", cantidad de consonantes: " + consonantes;
+ 	}
+ 	/**
+ 	 * Determina cuántas palabras tienen más letras que el número mínimo especificado.
+ 	 * 
+ 	 * @param frase La frase a comparar
+ 	 * @param numero El numero considerado como minimo
+ 	 * @return un String proporcionando la información del resultado
+ 	 */
+ 	public String contarPalabrasLargas(String frase, int numero) {
+         int contador = 0;
+         int largo = 0;
+         
+         for (int i = 0; i <= frase.length(); i++) {
+         	if(i < frase.length() && frase.charAt(i) != ' ') {
+         		largo += 1;
+         	}
+         	else if(largo > 0) {
+         		if(largo > numero) {
+         			contador += 1;
+         		}
+         		largo = 0;
+         	}
+ 			
+ 		}
+         return "cantidad de palabras con largo mayor a " + numero + " es: " + contador;
+ 
+ 	}
+ 	
 	/**
 	 * Este metodo verifica si el caracter es vocal o no
 	 * 
@@ -81,27 +106,28 @@ public class ContadorPalabras {
 		}
 		return false;
 	}
+	
 	/**
 	 * Lee el archivo y devuelve el contenido en un arreglo llamado lineas
 	 * 
 	 * @param archivo La ruta del archivo a leer
 	 * @return Arreglo de cadenas en el que el contenido son las lineas del archivo
 	 */
-	public String[] obtenerLineas(String archivo) {
-		List<String> lineas = new ArrayList<String>();)
-
-		try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
-			String linea = br.readLine();
-			while(linea != null) {
+	public String[] obtenerLineas(String archivo) throws IOException {
+		List<String> lineas = new ArrayList<String>();
+		String fileName = archivo;
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		try {
+			String linea;
+			while((linea = br.readLine()) != null) {
 				lineas.add(linea);
-				linea = br.readLine();
 			}
-		} 
-		catch (IOException e) {
-            System.out.println("Error al leer el archivo: " + e.getMessage());
-        }
+		} finally {
+		    br.close();
+		}
         return lineas.toArray(new String[0]);
-    }
+	}
+
 	/**
 	 * Cuenta la cantidad de palabras que contiene el conjunto de lineas
 	 * 
@@ -110,8 +136,8 @@ public class ContadorPalabras {
 	 */
 	public int cantidadPalabras(String[] lineasArchivo) {
 		int contador = 0;
-		for (String palabra : lineasArchivo) {
-			contador += contarPalabras(palabra);
+		for (String frase : lineasArchivo) {
+			contador = contador + contarPalabras(frase);
 		}
 		return contador;
 	}
--- src/uy/ucu/tp3/Principal.java
+++ src/uy/ucu/tp3/Principal.java
@@ -1,10 +1,14 @@
 package uy.ucu.tp3;
 
+import java.io.IOException;
+
 public class Principal {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		ContadorPalabras contador = new ContadorPalabras();
-//		contador.contarPalabras("hola mundo", 2);
-		contador.contarPalabras("hoy es un dia soleado 7", 2);
+		System.out.println("cantidad de palabras: " + contador.contarPalabras("hola mundo"));
+		System.out.println(contador.cantidadLetras(" hoy es un dia soleado " ));
+		System.out.println(contador.contarPalabrasLargas("voy a comer 4 galletas", 2));
+		System.out.println("cantidad de palabras: " + contador.cantidadPalabras(contador.obtenerLineas("archivo.txt")));
 	}
 }
